f02fff53387c963bc1349110f3f2240f4b0e7b2a
Ronan <binnyronan@gmail.com>
2021-08-01 22:46:22 +1200

    Initial setup of backend

---
+++ server/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const db = require('./db');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
